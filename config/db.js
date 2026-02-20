df89cde6c541aecc7965d959f6be096e99ab30e9
mahima-sharma <sharmamahima0510@gmail.com>
2026-02-20 12:38:43 +0530

    db changes

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 console.log("✅ Loaded DB config:");
 console.log("DB_HOST:", process.env.DB_HOST);
 console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD ? '****' : '(missing)');
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD ? 'GgzjDOf8smv1eRkL' : '(missing)');
 console.log("DB_NAME:", process.env.DB_NAME);
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
